cae90dbee811f3f9bcff6333f761d8f09c09519e
Ghostik512 <68357357+Ghostik512@users.noreply.github.com>
2022-12-02 11:49:55 +0700

    Create 21222.c

---
+++ 21222.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+char perevod(unsigned int a);
+void zaglav(char*simbol); -A();
+void stroch(char*simbol); -a();
+void swap(int*a,int*b); 
+int sum(int*a,int*b);
+int sub(int*a,int*b);
+double mult(double*a,double*b);
+float dev(float*a,float*b);
+void mass(void());
